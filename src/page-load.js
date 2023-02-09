0b20bbd9abcf165f1523e8d09a9b7f155719ca93
shmerv <eaudioworks@gmail.com>
2023-02-09 11:38:05 -0800

    Added fuctionality to page-load.js

--- src/page-load.js
+++ src/page-load.js
@@ -1,3 +1,20 @@
 export default function content() {
-    console.log('I am the content')
+
+    const contentElement  = document.getElementById('content');
+
+    const heading = document.createElement('h1');
+    heading.textContent = "Worst Restaurant in the World!";
+
+
+    const image = document.createElement('img');
+    image.src = "../media/img.jpeg";
+
+    const description = document.createElement('h3');
+    description.textContent = "Are you tired of gourmet meals and Michelin-star dining experiences? Come dine with us at the Worst Restaurant in the World! Our menu is a mystery, our ingredients questionable, and our chefs questionable, but one thing's for sure, you'll never forget your dining experience with us. And if you do, just ask our friendly waitstaff, they`ll be more than happy to remind you. Reservations not recommended, unless you`re brave enough to face the unknown. See you soon!" 
+
+    contentElement.appendChild(heading);
+    contentElement.appendChild(image);
+    contentElement.appendChild(description);
+
+
 }
